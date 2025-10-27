0df3206c851d9fbc309ad383ae6ca723fa9df802
Talha Rehman <itsme.talha64@gmail.com>
2025-10-27 11:21:18 +0500

    update file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,9 +42,6 @@
         </div>
     </section>
 
-    @if (dfsdfsafsdf)
-        
-    @endif
     <!-- Features Section -->
     <section id="features" class="py-20 bg-white">
         <div class="max-w-6xl mx-auto px-6 text-center">
